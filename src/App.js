468de0b0806b65318d45948b85468522ff1feb53
mdakhlaqueali <mdakhlaqueali@gmail.com>
2024-02-16 20:55:24 +0530

    Creating Signup and Login Screen

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import Navigation from './components/Navigation/Navigation';
 import './App.css';
+import AuthForm from './components/AuthForm/AuthForm';
+import HomePage from './components/Pages/HomePage';
+import { useContext } from 'react';
+import AuthContext from './store/auth-context';
+import {Route, Routes, Navigate} from 'react-router-dom';
+import Profile from './components/Pages/Profile';
 
 function App() {
+  const authCtx = useContext(AuthContext);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Navigation/>
+    <Routes>
+      <Route path='/' element={<HomePage/>}/>
+        {authCtx.isLoggedIn && <Route path="/profile" element={<Profile/>}/>}
+        {!authCtx.isLoggedIn && <Route path="/login" element={<AuthForm/>}/>}
+        <Route path="*" element={<Navigate to="/" />}/>
+      </Routes>
+    </>
   );
 }
 
---
+++ src/components/Pages/Profile.jsx
@@ -0,0 +1,6 @@
+const Profile = () => {
+    return(
+        <div>This is profile page</div>
+    )
+}
+export default Profile;
