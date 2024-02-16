f346aa83765b675465c198f39e3cf79f8aa4909b
mdakhlaqueali <mdakhlaqueali@gmail.com>
2024-02-16 21:18:12 +0530

    Adding Expenses for tracking purpose

---
+++ src/components/Pages/Expenses/ExpenseDetails.jsx
@@ -0,0 +1,13 @@
+const ExpenseDetails = (props) => {
+  const expenseList = props.expenses.map((expense) => {
+    return (
+      <li key={expense.id}>
+        {expense.amount} - {expense.description} - {expense.category}
+      </li>
+    );
+  });
+
+  return <ul>{expenseList}</ul>;
+};
+
+export default ExpenseDetails;
